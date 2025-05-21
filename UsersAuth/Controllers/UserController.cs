837a750ee2e934b0e2f5948be4589be4971bcbaf
elGatito <kaldisar098@gmail.com>
2025-05-21 20:21:17 +0300

    Add project files.

---
+++ UsersAuth/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class UserController : ControllerBase
+	{
+		[HttpGet(Name = "GetUser")]
+
+
+		[HttpGet(Name = "GetAllUsers")]
+
+		[HttpPut(Name = "UpdateUser")]
+
+		[HttpDelete(Name = "DeleteUser")]
+
+
+	}
+}
